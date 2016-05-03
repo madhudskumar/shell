f080806b46a79f77d9f7128537a2d0eaac710f7f
Madhu Kumar D S <Madhu Kumar D S>
2016-05-04 01:36:31 +0530

    test posix using macros

---
+++ testMacro.c
@@ -0,0 +1,22 @@
+#define _POSIX_SOURCE
+#define _POSIX_C_SOURCE 199309L
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+void check(char *name, int parm){
+	#ifdef parm
+		printf("\nSS");
+	#else
+		printf("\nSDS");
+	#endif
+	printf(" %s",name);
+}
+
+void main(){
+	check("job ctrl",_POSIX_JOB_CONTROL);
+       	check("saved ids",_POSIX_SAVED_IDS);
+	check("chown rest",_POSIX_CHOWN_RESTRICTED);
+	check("posix vd",_POSIX_VDISABLE);
+} 
